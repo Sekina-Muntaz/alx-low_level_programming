fd4dc460b025eb57abd0af61682efebf0e299b18
Sekina-Muntaz <sherzmuntaz@gmail.com>
2023-03-22 03:59:57 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
 /**
-*main - prints out first 98
+*main - prints sum of even fibonacci
+*suit elements
 *fibonacci suit numbers
 *Return: return 0
 */
 int main(void)
 {
-int inc;
-unsigned long n1 = 0, n2 = 1, n3;
-unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
-unsigned long h1, h2;
-
-for (inc = 0; inc < 92; inc++)
+unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+while (n3 <= 4000000)
 {
 n3 = n1 + n2;
-printf("%lu, ", n3);
 n1 = n2;
 n2 = n3;
-}
-n1_h1 = n1 / 10000000000;
-n2_h1 = n2 / 10000000000;
-n1_h2 = n1 % 10000000000;
-n2_h2 = n2 % 10000000000;
-for (inc = 93; inc < 99; inc++)
-{
-h1 = n1_h1 + n2_h1;
-h2 = n1_h2 + n2_h2;
-if ((n1_h2 + n2_h2) > 9999999999)
-{
-h1 += 1;
-h2 %= 10000000000;
-}
-printf("%lu%lu", h1, h2);
-if (inc != 98)
-printf(", ");
 
-n1_h1 = n2_h1;
-n1_h2 = n2_h2;
-n2_h1 = h1;
-n2_h2 = h2;
+if ((n1 % 2) == 0)
+sum += n1;
+
 }
-printf("\n");
+printf("%ld\n", sum);
 return (0);
 }
