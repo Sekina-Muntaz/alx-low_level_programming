8983ceda11ecfba4986a13567c1d229becc9e33f
Sekina-Muntaz <sherzmuntaz@gmail.com>
2023-05-03 01:03:30 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Write a function that returns the sum of all the data (n) of a listint_t linked list.
+ * @head: pointer to the list;
+ * Return: sum or 0 if list is empty.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
